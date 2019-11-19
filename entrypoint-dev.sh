8c329e1602b6c0a7da2a0c0b10d2a15f4ab80b69
Dustin Rue <dustin.rue@10up.com>
2019-11-18 23:15:04 -0600

    sudo these too

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -3,9 +3,9 @@
 ENABLE_XDEBUG=$(echo ${ENABLE_XDEBUG:-false} | tr '[:upper:]' '[:lower:]')
 
 if [ ${ENABLE_XDEBUG} == true ]; then
-  ln -fs /etc/php-extensions-available/15-xdebug.ini /etc/php.d/15-xdebug.ini
+  sudo ln -fs /etc/php-extensions-available/15-xdebug.ini /etc/php.d/15-xdebug.ini
 else
-  rm -f /etc/php.d/15-xdebug.ini
+  sudo rm -f /etc/php.d/15-xdebug.ini
 fi
 
 # Ensure we have the host.docker.internal hostname available to linux as well
